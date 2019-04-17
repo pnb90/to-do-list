950e5af8f5a6dc1b08af21a8bb079de17d490e41
pnb90 <phuocbui90@gmail.com>
2019-04-17 14:25:15 -0500

    css for to-do list

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ToDoItem() {
+  return(
+    <div className="todo-item">
+      <input type="checkbox"/>
+      <p>Do the dishes</p>
+    </div>
+  )
+}
+export default ToDoItem;
